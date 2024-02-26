f097aae109c50fbe17bd22278bb3a365c9d35183
Satya <gvvsn147@gmail.com>
2024-02-26 11:36:22 +0530

    pages centered

--- src/pages/TechEventLandingPage.jsx
+++ src/pages/TechEventLandingPage.jsx
@@ -8,14 +8,14 @@ const EventLandingPage = () => {
   return (
     <>
       <ScrollToTop />
-      <div className="bg-site bg-no-repeat bg-fixed overflow-hidden min-h-[100vh] pb-20 lg:pb-5">
+      <div className="bg-site bg-no-repeat bg-fixed overflow-hidden min-h-[100vh] pb-20">
         <Header />
         <div className="h2 text-center text-gradient">
             <h1>
               Tech events
             </h1>
         </div>
-        <div className="flex flex-col lg:flex-row flex-wrap justify-center items-center lg:mt-10">
+        <div className="flex flex-col lg:flex-row lg:flex-wrap justify-center items-center lg:mt-10">
           <EventCard/>
         </div>
         <HomeComp />
