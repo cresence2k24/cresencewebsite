50d54f61fb7066fb864093f672d483a12d16aa29
Satya <gvvsn147@gmail.com>
2024-02-27 21:47:58 +0530

    sponosrs added

--- src/components/SponsorComp.jsx
+++ src/components/SponsorComp.jsx
@@ -5,6 +5,11 @@
   import pennant from "../assets/Images/pennant.webp";
   import sukras from "../assets/Images/sukras.webp";
   import ssk from "../assets/Images/ssk.webp"; 
+  import svs from "../assets/Images/svs.webp";
+  import rk from "../assets/Images/rkacademy.webp";
+  import kritunga from "../assets/Images/kritunga.webp";
+  import hayat from "../assets/Images/hayat.webp";
+  import foodclock from "../assets/Images/foodclock.webp";
   
   const LOGOS = [
     <img src={pennant} alt="pennant" className="h-[100px] w-[200px] bg-white rounded-lg overflow-hidden"/>,
@@ -13,7 +18,12 @@
     <img src={shalimar} alt="shalimar" className="h-[100px] w-[200px] bg-white rounded-lg overflow-hidden"/>,
     <img src={sukras} alt="sukras" className="h-[100px] w-[200px] bg-white rounded-lg overflow-hidden"/>,
     <img src={ssk} alt="ssk" className="h-[100px] w-[125px] bg-white rounded-lg overflow-hidden"/>,
-   
+    <img src={svs} alt="svs" className="h-[100px] w-[125px] bg-white rounded-lg overflow-hidden"/>,
+    <img src={rk} alt="rk" className="h-[100px] w-[120px] bg-white rounded-lg overflow-hidden"/>,
+    <img src={kritunga} alt="kritunga" className="h-[100px] w-[200px] bg-white rounded-lg overflow-hidden"/>,
+    <img src={foodclock} alt="foodOclock" className="h-[100px] w-[200px] bg-white rounded-lg overflow-hidden"/>,
+    <img src={hayat} alt="hayat" className="h-[100px] w-[200px] bg-white rounded-lg overflow-hidden"/>,
+  
   ];
   
 const SponsorComp = () => {
@@ -24,7 +34,7 @@ const SponsorComp = () => {
             </Reveal>
       <div className="bg-transparent relative w-[100%] my-0 h-[100px] overflow-hidden flex justify-center items-center">
         
-        <div className="animate-infinite-slider flex gap-10 justify-center items-center w-[calc(250px*12)]">
+        <div className="animate-infinite-slider flex gap-10 justify-center items-center w-[calc(250px*15)]">
           {LOGOS.map((logo, index) => (
             <div
               className="slide flex w-[200px] items-center justify-center bg-white rounded-lg overflow-hidden"
--- src/components/SponsorsPageComp.jsx
+++ src/components/SponsorsPageComp.jsx
@@ -5,6 +5,11 @@ import shalimar from "../assets/Images/shalimar.webp";
 import pennant from "../assets/Images/pennant.webp";
 import sukras from "../assets/Images/sukras.webp";
 import ssk from "../assets/Images/ssk.webp";
+import svs from "../assets/Images/svs.webp";
+import rkacademy from "../assets/Images/rkacademy.webp";
+import kritunga from "../assets/Images/kritunga.webp";
+import hayat from "../assets/Images/hayat.webp";
+import foodclock from "../assets/Images/foodclock.webp";
 
 
 const Sponsors = () => {
@@ -21,10 +26,14 @@ const Sponsors = () => {
           </div>
 
 
-          <div class="flex flex-col mt-2 gap-y-10 lg:flex-row lg:flex-wrap items-center justify-center md:justify-around">
+          <div class="flex flex-col mt-2 gap-10 lg:flex-row lg:flex-wrap items-center justify-center">
 
             <div className="bg-white rounded-lg">
               <img src={pennant} alt="pennant"/>
+            </div>
+
+            <div class="overflow-hidden rounded-2xl h-[170px] w-[200px] lg:h-[120px] lg:w-[150px] flex items-center justify-center">
+              <img src={svs} alt="svsevents" />
             </div>
 
             <div className="overflow-hidden rounded-lg h-[150px] w-[250px] lg:h-[113px] lg:w-[150px] flex items-center justify-center">
@@ -46,9 +55,27 @@ const Sponsors = () => {
               <img src={sukras} alt="sukras" />
             </div>
 
-            <div className="h-[200px] w-[200px] lg:h-[150px] lg:w-[150px] flex items-center justify-center">
+            <div className="h-[200px] w-[200px] lg:h-[180px] lg:w-[180px] flex items-center justify-center">
               <img src={ssk} alt="ssk"/>
             </div>
+
+            <div class="overflow-hidden rounded-2xl h-[250px] w-[250px] lg:h-[200px] lg:w-[200px] flex items-center justify-center">
+              <img src={rkacademy} alt="rkacademy" />
+            </div>
+
+            <div class="overflow-hidden rounded-2xl h-[200px] w-[200px] lg:h-[150px] lg:w-[150px] flex items-center justify-center">
+              <img src={kritunga} alt="kritunga" />
+            </div>
+
+            <div class="overflow-hidden rounded-2xl h-[150px] w-[220px] lg:h-[100px] lg:w-[150px] flex items-center justify-center">
+              <img src={hayat} alt="hayat" />
+            </div>
+
+            <div class="overflow-hidden rounded-2xl h-[170px] w-[220px] lg:h-[150px] lg:w-[150px] flex items-center justify-center">
+              <img src={foodclock} alt="foodOclock" />
+            </div>
+            
+
 
             
           </div>
